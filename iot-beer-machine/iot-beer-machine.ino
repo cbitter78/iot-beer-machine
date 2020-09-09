fd9b76a300dd19af6816ee7de1dca74a3b17658b
Charles Bitter <charles_bitter@cable.comcast.net>
2020-09-09 18:40:33 -0400

    working

--- iot-beer-machine/iot-beer-machine.ino
+++ iot-beer-machine/iot-beer-machine.ino
@@ -7,16 +7,40 @@ AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 
 
 // Set up feeds.
-AdafruitIO_Feed *command   = io.feed("beer-machine.command");          // Used to recieve commands
-AdafruitIO_Feed *c_return  = io.feed("beer-machine.return");           // Used to reply to commands
-AdafruitIO_Feed *msg       = io.feed("beer-machine.msg");              // Used to send general string messages
-AdafruitIO_Feed *status    = io.feed("beer-machine.machine-status");   // Overall Machine Status (Ready, Vending, Open, Error)
-AdafruitIO_Feed *vend_rate = io.feed("beer-machine.vend_rate");        // Reported per can vending 
-AdafruitIO_Feed *in_temp   = io.feed("beer-machine.inner-temp");       // The temp inside the machine (next to the cans)
-AdafruitIO_Feed *out_temp  = io.feed("beer-machine.outside-temp");     // The temp outside the machine 
-AdafruitIO_Feed *out_quh   = io.feed("beer-machine.outside-quh");      // The Barometric Pressure outside the machine 
-AdafruitIO_Feed *out_rh    = io.feed("beer-machine.outside-rh");       // Relative Humidity outside the machine
-AdafruitIO_Feed *ac_amps   = io.feed("beer-machine.outside-ac-amps");  // Amps used by the AC Compressor
+AdafruitIO_Feed *command   = io.feed("iot-beer.command");          // Used to recieve commands
+AdafruitIO_Feed *c_return  = io.feed("iot-beer.return");           // Used to reply to commands
+AdafruitIO_Feed *msg       = io.feed("iot-beer.msg");              // Used to send general string messages
+AdafruitIO_Feed *m_status  = io.feed("iot-beer.machine-status");   // Overall Machine Status (Ready, Vending, Open, Error)
+AdafruitIO_Feed *vend_rate = io.feed("iot-beer.vend_rate");        // Reported per can vending 
+AdafruitIO_Feed *in_temp   = io.feed("iot-beer.inner-temp");       // The temp inside the machine (next to the cans)
+AdafruitIO_Feed *out_temp  = io.feed("iot-beer.outside-temp");     // The temp outside the machine 
+AdafruitIO_Feed *out_quh   = io.feed("iot-beer.outside-quh");      // The Barometric Pressure outside the machine 
+AdafruitIO_Feed *out_rh    = io.feed("iot-beer.outside-rh");       // Relative Humidity outside the machine
+AdafruitIO_Feed *ac_amps   = io.feed("iot-beer.outside-ac-amps");  // Amps used by the AC Compressor
+
+
+
+void init_feeds()
+{
+  AdafruitIO_Feed feeds [10] = {*command, *c_return, *msg, *m_status, *vend_rate, *in_temp, *out_temp, *out_quh, *out_rh, *ac_amps};
+
+  int n;
+  for ( n=0 ; n<10 ; ++n )
+  {
+    AdafruitIO_Feed f = feeds[n];
+    if (f.exists() == false){
+      Serial.print("Creating Feed:");
+      Serial.println(f.name);
+      f.create();
+    }
+    Serial.print("Setting Location on Feed:");
+    Serial.println(f.name);
+    f.setLocation(LOCATION_LAT, LOCATION_LONG, LOCATION_ALT);
+  }
+
+}
+
+
 
 void setup() {
 
@@ -35,7 +59,7 @@ void setup() {
   // the handleMessage function (defined below)
   // will be called whenever a message is
   // received from adafruit io.
-  command->onMessage(handleMessage);
+  command->onMessage(iotCommand);
 
   // wait for a connection
   while(io.status() < AIO_CONNECTED) {
@@ -46,7 +70,10 @@ void setup() {
   // we are connected
   Serial.println();
   Serial.println(io.statusText());
+  init_feeds();
   command->get();
+  m_status->save("Ready");
+  msg->save("Lets have a beer!");
 
 }
 
@@ -61,11 +88,11 @@ void iotCommand(AdafruitIO_Data *data){
 
   int v = data->toInt();
   if (v == 256){
-    status->save("Vending")
-    msg->save("About to vend a beer!")
+    m_status->save("Vending");
+    msg->save("About to vend a beer!");
     delay(3000);
     c_return->save(0);
-    msg->save("Beer is ready for you to pick up.  Cheers!")
-    status->save("Ready")
+    msg->save("Beer is ready for you to pick up.  Cheers!");
+    m_status->save("Ready");
   }
 }
