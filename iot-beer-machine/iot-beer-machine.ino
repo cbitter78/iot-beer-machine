5b5cae8b443adddc3275060e0e8b484f8c3732d7
Charles Bitter <cbitter78@aol.com>
2020-10-26 08:24:57 -0400

    working

--- iot-beer-machine/iot-beer-machine.ino
+++ iot-beer-machine/iot-beer-machine.ino
@@ -5,24 +5,16 @@
 #include <LiquidCrystal_I2C.h>
 #include "VendSlot.h"
 
+#include "util.h"
+
 //#include <ArduinoJson.h>  //https://arduinojson.org/v6/assistant/
 
 Adafruit_ADS1115 a2d(0x48);      
 LiquidCrystal_I2C lcd(0x27,20,4); 
- 
-#define BTN_1 A0
-#define BTN_2 A1 
-
+VendSlot* slots[2];
 VendSlot slot1;
 VendSlot slot2;
 
-
-VendSlot* slots[2];
- 
-
-
-
- 
 void setup(void)
 {
   Serial.begin(9600);
@@ -33,7 +25,6 @@ void setup(void)
   lcd.backlight();
 
 
-  Serial.println("About to set up Slots");
   slot1.setup(1, 12, &a2d, 0, &lcd, 0, 0);
   slot2.setup(2, 11, &a2d, 1, &lcd, 1, 0);
 
@@ -45,17 +36,10 @@ void setup(void)
 //    (*slots[i]).vend();
 //  }
 
-
 }
 
 void loop(void)
-{
-
-    Serial.print("Analog A0: ");
-    Serial.println(analogRead(A0));
-    Serial.print("Analog A1: ");
-    Serial.println(analogRead(A1));
-    
+{    
     if (analogRead(A0) > 100){
       (*slots[0]).vend();
     }
@@ -64,28 +48,7 @@ void loop(void)
       (*slots[1]).vend();
     }
   
-  //  print_adc(a2d, lcd);
+    print_adc(a2d, lcd);
     flash();
     delay(250);
-}
-
-
-void print_adc(Adafruit_ADS1115 b, LiquidCrystal_I2C lcd){
-  for (int i = 0; i <= 1; i++) {
-    int v = b.readADC_SingleEnded(i);
-    lcd.setCursor(0,i+2);
-    lcd.print(i);
-    lcd.print(": ");
-    lcd.print(v);
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.println(v);
-  }
-
-}
-
-void flash(){
-  digitalWrite(13, HIGH);
-  delay(250);
-  digitalWrite(13, LOW);
 }
--- iot-beer-machine/util.h
+++ iot-beer-machine/util.h
@@ -1,23 +1,39 @@
-//
-//#include "AdafruitIO_Feed.h"
-//
-//void init_feeds(AdafruitIO_Feed feeds)
-//{
-//  
-//
-//  int n;
-//  for ( n=0 ; n<10 ; ++n )
-//  {
-//    AdafruitIO_Feed f = feeds[n];
-//    if (f.exists() == false){
-//      Serial.print("Creating Feed:");
-//      Serial.println(f.name);
-//      f.create();
-//    }
-//    Serial.print("Setting Location on Feed:");
-//    Serial.println(f.name);
-//    Serial.println(f.owner);
-//    f.setLocation(LOCATION_LAT, LOCATION_LONG, LOCATION_ALT);
-//  }
-//
-//}
+#include <Adafruit_ADS1015.h>
+
+void print_adc(Adafruit_ADS1115 b, LiquidCrystal_I2C lcd){
+  int x = 3;
+  int ports[x + 1];
+  
+  for (int i = 0; i <= x; i++) {
+    int v = b.readADC_SingleEnded(i);
+    if (v == 65535){ v = 0; }
+    ports[i] = v;
+  }
+
+  lcd.setCursor(0,2);
+  lcd.print("                   ");
+  lcd.setCursor(0,3);
+  lcd.print("                   ");
+  
+  lcd.setCursor(0,2);
+  lcd.print("0:" + String(ports[0]));
+  Serial.println("ADC:0:" + String(ports[0]));
+  
+  lcd.setCursor(10,2);
+  lcd.print("1:" + String(ports[1]));
+  Serial.println("ADC:1:" + String(ports[1]));
+
+  lcd.setCursor(0,3);
+  lcd.print("2:" + String(ports[2]));
+  Serial.println("ADC:2:" + String(ports[2]));
+
+  lcd.setCursor(10,3);
+  lcd.print("3:" + String(ports[3]));
+  Serial.println("ADC:" + String(ports[3]));
+}
+
+void flash(){
+  digitalWrite(13, HIGH);
+  delay(250);
+  digitalWrite(13, LOW);
+}
