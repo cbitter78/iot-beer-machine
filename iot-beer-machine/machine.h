972ba1a0a0fe9890b1ecadb17b07567fc2429d82
Charles Bitter <cbitter78@aol.com>
2020-11-13 22:25:16 -0500

    working

---
+++ iot-beer-machine/machine.h
@@ -0,0 +1,40 @@
+
+#define ADC_0_ADDRESS 0x48
+#define ADC_0_ADDRESS 0x49
+#define ADC_0_ADDRESS 0x4A
+
+#define SLOT_1_DIO_PIN 9
+#define SLOT_2_DIO_PIN 10
+#define SLOT_3_DIO_PIN 11
+#define SLOT_4_DIO_PIN 12
+#define SLOT_5_DIO_PIN 5
+#define SLOT_6_DIO_PIN 6
+
+#define SLOT_1_VEND_ADC 1 
+#define SLOT_2_VEND_ADC 1 
+#define SLOT_3_VEND_ADC 1 
+#define SLOT_4_VEND_ADC 1 
+#define SLOT_5_VEND_ADC 0 
+#define SLOT_6_VEND_ADC 0 
+
+#define SLOT_1_VEND_ADC_PIN 0
+#define SLOT_2_VEND_ADC_PIN 1
+#define SLOT_3_VEND_ADC_PIN 2
+#define SLOT_4_VEND_ADC_PIN 3
+#define SLOT_5_VEND_ADC_PIN 0
+#define SLOT_6_VEND_ADC_PIN 1
+
+#define SLOT_1_EMPTY_ADC 0 
+#define SLOT_2_EMPTY_ADC 0 
+#define SLOT_3_EMPTY_ADC 2 
+#define SLOT_4_EMPTY_ADC 2 
+#define SLOT_5_EMPTY_ADC 2 
+#define SLOT_6_EMPTY_ADC 2 
+
+
+#define SLOT_1_EMPTY_ADC_PIN 2
+#define SLOT_2_EMPTY_ADC_PIN 3
+#define SLOT_3_EMPTY_ADC_PIN 0
+#define SLOT_4_EMPTY_ADC_PIN 1
+#define SLOT_5_EMPTY_ADC_PIN 2
+#define SLOT_6_EMPTY_ADC_PIN 3
